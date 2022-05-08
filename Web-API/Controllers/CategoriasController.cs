7c29301f2de863a3819c9d940f88e4264d0a75a3
carlos.santos <carlos.santos@sp.agence.com.br>
2022-05-08 12:53:57 -0300

    Criando estrutura para tornar a API RestFul

---
+++ Web-API/Controllers/CategoriasController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCategorias()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult PostCategorias()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult PutCategorias()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCategorias()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Web-API/Controllers/ProdutosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetProdutos()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult PostProdutos()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult PutProdutos()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProdutos()
+        {
+            return Ok();
+        }
+
+    }
+}
